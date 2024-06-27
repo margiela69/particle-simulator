87b57376a5fcff218f0585e42b01150bb98eb266
Christoffer <christoffergravej@gmail.com>
2024-06-27 21:19:44 +0200

    added collision

--- particle.c
+++ particle.c
@@ -8,7 +8,7 @@
 #define SCREENWIDTH 800
 #define SCREENHEIGHT 800
 
-#define MAX_PARTICLES 100
+#define MAX_PARTICLES 500
 #define PARTICLE_RADIUS 10
 
 typedef struct Particle
@@ -21,18 +21,23 @@ typedef struct Particle
 
 Vector2 gravity = {0.0f, 1000.0f};
 
-void updatePosition(Particle* particle, float dt)
+void updatePositions(Particle* particles, float dt, int* count)
 {
-    Vector2 velocity = {(particle->pos_cur.x - particle->pos_old.x), (particle->pos_cur.y - particle->pos_old.y)};
+    for (int i = 0; i < *count; i++)
+    {
+        Particle* particle = &(particles[i]);
 
-    particle->pos_old = particle->pos_cur;
+        Vector2 velocity = {(particle->pos_cur.x - particle->pos_old.x), (particle->pos_cur.y - particle->pos_old.y)};
 
-    // pos_cur = pos_cur + velocity + acceleration * dt * dt
-    particle->pos_cur.x += velocity.x + (particle->acceleration.x * dt * dt);
-    particle->pos_cur.y += velocity.y + (particle->acceleration.y * dt * dt);
+        particle->pos_old = particle->pos_cur;
 
-    particle->acceleration.x = 0;
-    particle->acceleration.y = 0;
+        // pos_cur = pos_cur + velocity + acceleration * dt * dt
+        particle->pos_cur.x += velocity.x + (particle->acceleration.x * dt * dt);
+        particle->pos_cur.y += velocity.y + (particle->acceleration.y * dt * dt);
+
+        particle->acceleration.x = 0;
+        particle->acceleration.y = 0;
+    }
 }
 
 void accelerate(Particle* particle, Vector2 acc)
@@ -41,27 +46,42 @@ void accelerate(Particle* particle, Vector2 acc)
     particle->acceleration.y += acc.y;
 }
 
-void applyConstraint(Particle* particle)
+void applyGravity(Particle* particles, int* count)
 {
-    Vector2 center = {400.0f, 400.0f};
-    float circle_radius = 375.0f;
-
-    Vector2 to_center = Vector2Subtract(particle->pos_cur, center);
-    float distance = Vector2Length(to_center);
-
-    if (distance > circle_radius - PARTICLE_RADIUS)
+    for (int i = 0; i < *count; i++)
     {
-        Vector2 n = {(to_center.x / distance), (to_center.y / distance)};
-        particle->pos_cur.x = center.x + n.x * (circle_radius - PARTICLE_RADIUS);
-        particle->pos_cur.y = center.y + n.y * (circle_radius - PARTICLE_RADIUS);
+        accelerate(&(particles[i]), gravity);
     }
+}
+
+void applyConstraint(Particle* particles, int* count)
+{
+    for (int i = 0; i < *count; i++)
+    {
+        Particle* particle = &(particles[i]);
+
+        Vector2 center = {400.0f, 400.0f};
+        float circle_radius = 375.0f;
+
+        Vector2 to_center = Vector2Subtract(particle->pos_cur, center);
+        float distance = Vector2Length(to_center);
+
+        if (distance > circle_radius - PARTICLE_RADIUS)
+        {
+            Vector2 n = {(to_center.x / distance), (to_center.y / distance)};
+            particle->pos_cur.x = center.x + n.x * (circle_radius - PARTICLE_RADIUS);
+            particle->pos_cur.y = center.y + n.y * (circle_radius - PARTICLE_RADIUS);
+        }
+    }
+    
+
 }
 
 void solveCollision(Particle* particles, int* count)
 {
     for (int i = 0; i < *count; ++i)
     {
-        Particle* particle1 = &particles[i];
+        Particle* particle1 = &(particles[i]);
 
         for (int j = i+1; j < *count; ++j)
         {
@@ -69,19 +89,32 @@ void solveCollision(Particle* particles, int* count)
             Vector2 collision_axis = Vector2Subtract(particle1->pos_cur, particle2->pos_cur);
             float distance = Vector2Length(collision_axis);
 
-            if (distance < (float)PARTICLE_RADIUS)
+            if (distance < (float)PARTICLE_RADIUS * 2)
             {
-                float delta = (float)PARTICLE_RADIUS * 2 - distance;
-                //Vector2 n = {((collision_axis.x / distance) * ((float)PARTICLE_RADIUS / 100) * delta), ((collision_axis.y / distance) * ((float)PARTICLE_RADIUS / 100) * delta)};
-                Vector2 n = {((collision_axis.x / distance) * 0.05f * delta), ((collision_axis.y / distance) * 0.05f * delta)};
+                float delta = ((float)PARTICLE_RADIUS * 2) - distance;
+
+                Vector2 n = {((collision_axis.x / distance) * 0.5f * delta), ((collision_axis.y / distance) * 0.5f * delta)};
 
                 particle1->pos_cur.x += n.x;
                 particle1->pos_cur.y += n.y;
-
                 particle2->pos_cur.x -= n.x;
                 particle2->pos_cur.y -= n.y;
             }
         }
+    }
+}
+
+void update(Particle* particles, int* count, float dt)
+{
+    int sub_steps = 8;
+    float sub_dt = dt / (float)sub_steps;
+
+    for (int i = 0; i < sub_steps; i++)
+    {
+        applyGravity(particles, count);
+        applyConstraint(particles, count);
+        solveCollision(particles, count);
+        updatePositions(particles, sub_dt, count);
     }
 }
 
@@ -166,20 +199,14 @@ int main(void)
 
             addParticle(randParticle, particles, &counter, MAX_PARTICLES);
         }
-        
-        for (int i = 0; i < counter; i++) //update
-        {
-                accelerate(&particles[i], gravity);
-                applyConstraint(&particles[i]);
-                solveCollision(particles, &counter);
-                updatePosition(&particles[i], dt);
-        }
+
+        update(particles, &counter, dt);
 
         BeginDrawing();
         ClearBackground(DARK);
 
-        DrawCircleLines(SCREENWIDTH / 2, SCREENHEIGHT / 2, 375, GRAY);
-        DrawCircle(SCREENWIDTH / 2, SCREENHEIGHT / 2, 3, RED);
+        DrawCircleLines(SCREENWIDTH / 2, SCREENHEIGHT / 2, 375, GRAY); //draw constraint
+        //DrawCircle(SCREENWIDTH / 2, SCREENHEIGHT / 2, 3, RED); //center dot
         //DrawRectangleLines(100, 100, 600, 600, GRAY);
         //DrawLine(SCREENWIDTH / 2, 0, SCREENWIDTH / 2, SCREENHEIGHT, GRAY);
         //DrawLine(0, SCREENHEIGHT / 2, SCREENWIDTH, SCREENHEIGHT / 2, GRAY);
